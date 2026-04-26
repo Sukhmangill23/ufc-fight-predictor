bc87d6e8c56f2e039ac25c6fe8cefbc2253990eb
gillsu27 <sukhman.gill@mail.utoronto.ca>
2026-04-26 14:53:29 -0400

    feat(auth): move hardcoded secrets to env vars, fix CORS for production

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,12 +7,15 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['JWT_SECRET_KEY'] = 'your_strong_secret_key_here'
+
+    # JWT config
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'dev_secret_change_in_prod')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 
     CORS(app, resources={
         r"/*": {
-            "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
+            "origins": ["http://localhost:3000", "http://127.0.0.1:3000",
+                        os.environ.get('FRONTEND_URL', '')],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
         }
@@ -23,11 +26,11 @@ def create_app():
 
     @app.after_request
     def add_cors_headers(response):
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        origin = os.environ.get('FRONTEND_URL', 'http://localhost:3000')
+        response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
         response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
         return response
 
     jwt = JWTManager(app)
-
     return app
